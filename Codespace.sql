39d7303660917891542ad1061b47ca8d044a2bba
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-08-27 13:54:58 +0200

    Advanced join

--- Codespace.sql
+++ Codespace.sql
@@ -1405,3 +1405,65 @@ SELECT
        NTILE(100) OVER (PARTITION BY account_id ORDER BY total_amt_usd) AS total_percentile
   FROM orders 
  ORDER BY account_id DESC
+
+-- Say you're an analyst at Parch & Posey and you want to see:
+
+-- each account who has a sales rep and each sales rep that has an account (all of the columns in these returned rows will be full)
+-- but also each account that does not have a sales rep and each sales rep that does not have an account (some of the columns in these returned rows will be empty)
+
+SELECT *
+  FROM accounts
+ FULL JOIN sales_reps ON accounts.sales_rep_id = sales_reps.id
+
+-- If unmatched rows existed (they don't for this query), you could isolate them by adding the following line to the end of the query:
+
+WHERE accounts.sales_rep_id IS NULL OR sales_reps.id IS NULL
+
+-- To elaborate on the rarity of FULL OUTER JOINS in practice, this Stack Overflow answer is helpful: When is a good situation to use a full outer join?
+
+-- In the following SQL Explorer, write a query that left joins the accounts table and the sales_reps tables on each sale rep's ID number and joins it using the < comparison operator on accounts.primary_poc and sales_reps.name, like so:
+
+accounts.primary_poc < sales_reps.name
+
+SELECT accounts.name as account_name,
+       accounts.primary_poc as poc_name,
+       sales_reps.name as sales_rep_name
+  FROM accounts
+  LEFT JOIN sales_reps
+    ON accounts.sales_rep_id = sales_reps.id
+   AND accounts.primary_poc < sales_reps.name
+
+-- Modify the query from the previous video, which is pre-populated in the SQL Explorer below, to perform the same interval analysis except for the web_events table. Also:
+
+-- change the interval to 1 day to find those web events that occurred after, but not more than 1 day after, another web event
+-- add a column for the channel variable in both instances of the table in your query
+
+SELECT o1.id AS o1_id,
+       o1.account_id AS o1_account_id,
+       o1.occurred_at AS o1_occurred_at,
+       o2.id AS o2_id,
+       o2.account_id AS o2_account_id,
+       o2.occurred_at AS o2_occurred_at
+  FROM orders o1
+ LEFT JOIN orders o2
+   ON o1.account_id = o2.account_id
+  AND o2.occurred_at > o1.occurred_at
+  AND o2.occurred_at <= o1.occurred_at + INTERVAL '28 days'
+ORDER BY o1.account_id, o1.occurred_at
+
+-- Modified
+
+SELECT we1.id AS we_id,
+       we1.account_id AS we1_account_id,
+       we1.occurred_at AS we1_occurred_at,
+       we1.channel AS we1_channel,
+       we2.id AS we2_id,
+       we2.account_id AS we2_account_id,
+       we2.occurred_at AS we2_occurred_at,
+       we2.channel AS we2_channel
+  FROM web_events we1 
+ LEFT JOIN web_events we2
+   ON we1.account_id = we2.account_id
+  AND we1.occurred_at > we2.occurred_at
+  AND we1.occurred_at <= we2.occurred_at + INTERVAL '1 day'
+ORDER BY we1.account_id, we2.occurred_at
