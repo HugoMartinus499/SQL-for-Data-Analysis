968f7bc855e5ad08b080e9a65f63647f2b7e2b19
Hugo von Essen-Müller <hugo.von.essen@gmail.com>
2024-09-04 19:14:24 +0200

    Union and performance tuning

--- Codespace.sql
+++ Codespace.sql
@@ -1467,3 +1467,41 @@ SELECT we1.id AS we_id,
   AND we1.occurred_at > we2.occurred_at
   AND we1.occurred_at <= we2.occurred_at + INTERVAL '1 day'
 ORDER BY we1.account_id, we2.occurred_at
+
+-- Write a query that uses UNION ALL on two instances (and selecting all columns) of the accounts table. Then inspect the results and answer the subsequent quiz.
+SELECT *
+    FROM accounts
+
+UNION ALL
+
+SELECT *
+  FROM accounts
+
+-- Add a WHERE clause to each of the tables that you unioned in the query above, filtering the first table where name equals Walmart and filtering the second table where name equals Disney. Inspect the results then answer the subsequent quiz.
+SELECT *
+    FROM accounts
+    WHERE name = 'Walmart'
+
+UNION ALL
+
+SELECT *
+  FROM accounts
+  WHERE name = 'Disney'
+
+-- Perform the union in your first query (under the Appending Data via UNION header) in a common table expression and name it double_accounts. 
+-- Then do a COUNT the number of times a name appears in the double_accounts table. If you do this correctly, your query results should have a count of 2 for each name.
+WITH double_accounts AS (
+    SELECT *
+      FROM accounts
+    
+    UNION ALL
+    
+    SELECT *
+      FROM accounts
+)
+
+SELECT name,
+       COUNT(*) AS name_count
+ FROM double_accounts 
+GROUP BY 1
+ORDER BY 2 DESC
